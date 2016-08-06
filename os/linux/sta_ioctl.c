75df4e5535ad09c71bf865bcd56f67b777a3bf11
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-08-06 19:49:15 +0200

    remove iwpriv table
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os/linux/sta_ioctl.c
+++ os/linux/sta_ioctl.c
@@ -1932,38 +1932,6 @@ static const iw_handler rt_handler[] =
 	(iw_handler) rt_ioctl_siwpmksa,         /* SIOCSIWPMKSA  */
 };
 
-static const iw_handler rt_priv_handlers[] = {
-	(iw_handler) NULL, /* + 0x00 */
-	(iw_handler) NULL, /* + 0x01 */
-	(iw_handler) NULL, /* + 0x02 */
-#ifdef DBG
-	(iw_handler) NULL, /* + 0x03 */
-#else
-	(iw_handler) NULL, /* + 0x03 */
-#endif
-	(iw_handler) NULL, /* + 0x04 */
-	(iw_handler) NULL, /* + 0x05 */
-	(iw_handler) NULL, /* + 0x06 */
-	(iw_handler) NULL, /* + 0x07 */
-	(iw_handler) NULL, /* + 0x08 */
-	(iw_handler) NULL, /* + 0x09 */
-	(iw_handler) NULL, /* + 0x0A */
-	(iw_handler) NULL, /* + 0x0B */
-	(iw_handler) NULL, /* + 0x0C */
-	(iw_handler) NULL, /* + 0x0D */
-	(iw_handler) NULL, /* + 0x0E */
-	(iw_handler) NULL, /* + 0x0F */
-	(iw_handler) NULL, /* + 0x10 */
-	(iw_handler) NULL, /* + 0x11 */
-    (iw_handler) NULL, /* + 0x12 */
-	(iw_handler) NULL, /* + 0x13 */
-    (iw_handler) NULL, /* + 0x14 */
-	(iw_handler) NULL, /* + 0x15 */
-    (iw_handler) NULL, /* + 0x16 */
-	(iw_handler) NULL, /* + 0x17 */
-	(iw_handler) NULL, /* + 0x18 */
-};
-
 const struct iw_handler_def rt28xx_iw_handler_def =
 {
 #define	N(a)	(sizeof (a) / sizeof (a[0]))
