80d631a1a24b84503bb9916937a6886c793f53e3
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-09-29 16:18:24 +0200

    fix in ->dev from stupid RTMP_OS_PKT_INIT()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/os/rt_linux.h
+++ include/os/rt_linux.h
@@ -772,7 +772,7 @@ void linux_pci_unmap_single(void *handle, dma_addr_t dma_addr, size_t size, int 
 {																			\
 	struct sk_buff * __pRxPkt;													\
 	__pRxPkt = RTPKT_TO_OSPKT(__pRxPacket);									\
-	__pRxPkt->len = __pNetDev;									\
+	__pRxPkt->dev = __pNetDev;									\
 	__pRxPkt->data = __pData;									\
 	__pRxPkt->len = __DataSize;									\
 	SET_OS_PKT_DATATAIL(__pRxPkt, __pData, __DataSize);						\
