9c7180ac1320a19dd8d73813e42985163a8c4257
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-10-01 15:10:21 +0200

    fix warning in call of dump_txinfo()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- common/cmm_data_usb.c
+++ common/cmm_data_usb.c
@@ -1006,7 +1006,7 @@ int RtmpUSBMgmtKickOut(
 //+++Add by shiang for debug
 if (0) {
 	DBGPRINT(RT_DEBUG_OFF, ("-->%s():shiang-6590, QueIdx=%d, SrcBufLen=%d\n", __FUNCTION__, QueIdx, SrcBufLen));
-	dump_txinfo(pAd, pTxInfo);
+	dump_txinfo(pAd, &pTxInfo->txinfo_nmac_pkt);;
 	dumpTxWI(pAd, (struct txwi_nmac *)(pSrcBufVA + TXINFO_SIZE));
 }
 //---Add by shiang for debug
