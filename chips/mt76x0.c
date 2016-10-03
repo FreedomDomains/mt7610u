8ad3c217b4acd3bbf44c8a17f5ede94109b4c9a2
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-10-03 12:17:03 +0200

    fix forbids mixed declarations due double semicolon
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- chips/mt76x0.c
+++ chips/mt76x0.c
@@ -2313,7 +2313,7 @@ void mt76x0_read_per_rate_tx_pwr(
 	IN struct rtmp_adapter *pAd)
 {
 	u32 data;
-	USHORT e2p_val = 0, e2p_val2 = 0;;
+	USHORT e2p_val = 0, e2p_val2 = 0;
 	u8 bw40_gband_delta = 0, bw40_aband_delta = 0, bw80_aband_delta = 0;
 	CHAR t1 = 0, t2 = 0, t3 = 0, t4 = 0;
 	BOOLEAN dec_aband_bw40_delta = FALSE, dec_aband_bw80_delta = FALSE, dec_gband_bw40_delta = FALSE;
