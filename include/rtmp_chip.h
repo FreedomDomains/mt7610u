49a6c283005f1f939f5bf08a1ffa70b3158d682a
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-01-19 20:54:42 +0100

    remove unused RtmpChipBcnInit(), detected while working at mac80211
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtmp_chip.h
+++ include/rtmp_chip.h
@@ -734,7 +734,6 @@ do {	\
 } while (0)
 
 int RtmpChipOpsHook(void *pCB);
-void RtmpChipBcnInit(struct rtmp_adapter *pAd);
 void RtmpChipBcnSpecInit(struct rtmp_adapter *pAd);
 void rlt_bcn_buf_init(struct rtmp_adapter *pAd);
 
