79ddd70964168ce81993da8f9637ff85f56d31ec
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-03-25 13:47:41 +0100

    remove unused dev_adjust_radio()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- mgmt/mgmt_hw.c
+++ mgmt/mgmt_hw.c
@@ -27,32 +27,3 @@
 
 #include <rt_config.h>
 
-
-INT dev_adjust_radio(struct rtmp_adapter*pAd)
-{
-	struct hw_setting *hw_cfg = &pAd->hw_cfg, new_cfg;
-
-
-	memset(&new_cfg, 0, sizeof(struct hw_setting));
-
-
-	/* For all wdev, find the maximum inter-set */
-
-
-	if (hw_cfg->bbp_bw != new_cfg.bbp_bw)
-	{
-		mt7610u_bbp_set_bw(pAd, new_cfg.bbp_bw);
-		hw_cfg->bbp_bw = new_cfg.bbp_bw;
-	}
-
-	if (hw_cfg->cent_ch != new_cfg.cent_ch)
-	{
-		u8 ext_ch = EXTCHA_NONE;
-
-		mt7610u_bbp_set_ctrlch(pAd, ext_ch);
-		mt7610u_mac_set_ctrlch(pAd, ext_ch);
-	}
-
-	return true;
-}
-
